72b95a35bc5863e012fa11848695e651978ed115
Ariotee <mat_990011@mail.ru>
2025-06-04 02:26:18 +0600

    Создание роутов

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,5 +1,9 @@
 import { createRoot } from 'react-dom/client'
-import App from '../App.jsx'
+import { route } from './routes/route.js'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(<App />)
+import { RouterProvider } from 'react-router'
+
+createRoot(document.getElementById('root')).render(
+	<RouterProvider router={route} />
+)
